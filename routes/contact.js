5bfbeb1df1833e9f881beb24dd651be6d3afa9a4
greg3560 <gregorypeigne1@gmail.com>
2022-08-13 23:20:07 +0200

    install axios + call api and management response for front

--- routes/contact.js
+++ routes/contact.js
@@ -1,9 +1,42 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios');
 
-/* GET users listing. */
+/* GET contact form. */
 router.post('/', function(req, res, next) {
-  res.render('index', { title: 'Accueil', showPopinConfirmation: true });
+  const firstName = req.body['first-name'];
+  let apiEndpoint = "https://gender-api.com/v2/gender";
+  let authToken = "d4c9610fa14cee57105dffba60319016269d09c70238e4c454ab6e0f066060b9";
+  let gender = "";
+  const data ={
+    "first_name": firstName
+  };
+  const config = {
+    headers:{
+      "Content-Type": "application/json",
+      "Authorization": "Bearer " + authToken
+    }
+  };
+
+  axios.post(apiEndpoint, data, config)
+      .then(function (response) {
+        if (response.data.result_found) {
+          if ('female' === response.data.gender) {
+            gender = "We rob banks";
+          } else if('male' === response.data.gender) {
+            gender = "Rosebud";
+          }
+        } else {
+          gender = "It’s a trap ! ";
+        }
+      }).then(function() {
+    res.render('index', { title: 'Accueil', showPopinConfirmation: true, gender });
+  })
+      .catch(function (error) {
+        gender = "It’s a trap ! ";
+        console.log('error', error);
+        res.render('index', { title: 'Accueil', showPopinConfirmation: true, gender });
+      });
 });
 
 module.exports = router;
